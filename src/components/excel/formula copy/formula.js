e16f6f9e10ae5a4216fb7631e0cc6f1f28169239
pdemlv <glvertex2@gmail.com>
2021-02-26 16:51:31 +0200

    excel components base structure

---
+++ src/components/excel/formula copy/formula.js
@@ -0,0 +1,5 @@
+import { ExcelComponent } from '../../formula/node_modules/@core/ExcelComponent'
+
+export class Formula extends ExcelComponent {
+
+}
---
+++ src/components/header/Header.js
@@ -0,0 +1,7 @@
+import { ExcelComponent } from "../../core/ExcalComponent";
+
+import { ExcelComponent } from '../formula/node_modules/@core/ExcelComponent'
+
+export class Header extends ExcelComponent {
+
+}
