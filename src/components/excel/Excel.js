d8c1fbf3e9fca4139af26fead34c29fd1ea27c51
pdemlv <glvertex2@gmail.com>
2021-02-27 13:18:52 +0200

    add method for creating elements and adding classes

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,3 +1,4 @@
+import { $ } from '@core/dom'
 export class Excel {
   constructor(selector, options) {
     this.$el = document.querySelector(selector)
@@ -5,12 +6,10 @@ export class Excel {
   }
 
   getRoot() {
-    const $root = document.createElement('div')
-    $root.classList.add('excel')
+    const $root = $.create('div', 'excel')
 
     this.components.forEach(Component => {
-      const $el = document.createElement('div')
-      $el.classList.add(Component.className)
+      const $el = $.create('div', Component.className)
       const component = new Component($el)
       $el.innerHTML = component.toHTML()
       $root.append($el)
