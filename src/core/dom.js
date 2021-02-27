95e672f949732597f37dcdbdec6b9f2f0b7a5475
pdemlv <glvertex2@gmail.com>
2021-02-27 15:12:34 +0200

    generate listeners for Formula

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,8 +1,41 @@
 class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string' ?
+      document.querySelector(selector) : selector
+  }
+
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html
+      return this
+    }
+    return this.$el.outerHTML.trim()
+  }
+
+  clear() {
+    this.html('')
+    return this
+  }
+
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback)
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el
+    }
+    if (Element.prototype.append) {
+      this.$el.append(node)
+    } else {
+      this.$el.appendChild(node)
+    }
+    return this
+  }
 }
 
-export function $() {
-  return new Dom()
+export function $(selector) {
+  return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
@@ -11,5 +44,5 @@ $.create = (tagName, classes = '') => {
     el.classList.add(classes)
   }
 
-  return el
+  return $(el)
 }
