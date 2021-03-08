3b0a03c81c61333b9a946246cd58ed4fadefc7ba
pdemlv <glvertex2@gmail.com>
2021-03-08 14:49:39 +0200

    logn resize line for row and columns

--- src/core/dom.js
+++ src/core/dom.js
@@ -52,6 +52,10 @@ class Dom {
   findAll(selector) {
     return this.$el.querySelectorAll(selector)
   }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => this.$el.style[key] = styles[key])
+  }
 }
 export function $(selector) {
   return new Dom(selector)
