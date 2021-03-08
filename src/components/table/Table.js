83843067a268199cee49bcde0265340ce88b1be0
pdemlv <glvertex2@gmail.com>
2021-03-08 13:40:53 +0200

    fix perfomance on resizing

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -19,13 +19,13 @@ export class Table extends ExcelComponent {
       const $resizer = $(event.target)
       const $parent = $resizer.closest('[data-type="resizable"]')
       const coords = $parent.getCoords()
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
 
       document.onmousemove = e => {
         const delta = Math.floor(e.pageX - coords.right)
         const value = coords.width + delta
         $parent.$el.style.width = value + 'px'
-        document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
-          .forEach(el => el.style.width = value + 'px')
+        cells.forEach(el => el.style.width = value + 'px')
       }
 
       document.onmouseup = () => {
--- src/core/dom.js
+++ src/core/dom.js
@@ -48,6 +48,10 @@ class Dom {
   getCoords() {
     return this.$el.getBoundingClientRect()
   }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
 }
 export function $(selector) {
   return new Dom(selector)
