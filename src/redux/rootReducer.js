1adaa097901397b64f9fb9012e6aecc0961e2dda
pdemlv <glvertex2@gmail.com>
2021-04-10 13:32:25 +0300

    add last date of opened table

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,7 +3,8 @@ import {
   CHANGE_STYLES,
   TABLE_RESIZE,
   APPLY_STYLE,
-  CHANGE_TITLE
+  CHANGE_TITLE,
+  UPDATE_DATE
 } from './types'
 
 export function rootReducer(state, action) {
@@ -12,7 +13,7 @@ export function rootReducer(state, action) {
   switch (action.type) {
     case TABLE_RESIZE:
       field = action.data.type === 'col' ? 'colState' : 'rowState'
-      return {...state, [field]: value(state, field, action)}
+      return { ...state, [field]: value(state, field, action) }
     case CHANGE_TEXT:
       field = 'dataState'
       return {
@@ -21,20 +22,22 @@ export function rootReducer(state, action) {
         [field]: value(state, field, action)
       }
     case CHANGE_STYLES:
-      return {...state, currentStyles: action.data}
+      return { ...state, currentStyles: action.data }
     case APPLY_STYLE:
       field = 'stylesState'
       val = state[field] || {}
       action.data.ids.forEach(id => {
-        val[id] = {...val[id], ...action.data.value}
+        val[id] = { ...val[id], ...action.data.value }
       })
       return {
         ...state,
         [field]: val,
-        currentStyles: {...state.currentStyles, ...action.data.value}
+        currentStyles: { ...state.currentStyles, ...action.data.value }
       }
     case CHANGE_TITLE:
-      return {...state, title: action.data}
+      return { ...state, title: action.data }
+    case UPDATE_DATE:
+      return { ...state, openedDate: new Date().toJSON() }
     default: return state
   }
 }
